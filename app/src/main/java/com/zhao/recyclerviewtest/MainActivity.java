97fde260b94f77f0e2d232bbd0400acf1e38ab5a
zhao <zhao199606@gamil.com>
2017-02-15 21:30:48 +0800

    second commit

--- app/src/main/java/com/zhao/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/zhao/recyclerviewtest/MainActivity.java
@@ -2,6 +2,9 @@ package com.zhao.recyclerviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    class RecyAdapter extends RecyclerView.Adapter<RecyAdapter.ViewHolder> {
+
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+
+    }
+
 }
+
