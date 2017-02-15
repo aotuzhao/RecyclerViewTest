5a75fe6fa51b980504e6230500c14abd2a3fd7c9
zhao <zhao199606@gmail.com>
2017-02-15 23:24:07 +0800

    third commit

--- app/src/main/java/com/zhao/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/zhao/recyclerviewtest/MainActivity.java
@@ -3,10 +3,18 @@ package com.zhao.recyclerviewtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<String> list;
+    private RecyAdapter recyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +27,20 @@ public class MainActivity extends AppCompatActivity {
 
         class ViewHolder extends RecyclerView.ViewHolder {
 
+            TextView tv;
+
             public ViewHolder(View itemView) {
                 super(itemView);
+                tv = (TextView) itemView.findViewById(R.id.id_num);
             }
         }
 
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.item, parent, false);
+            ViewHolder viewHolder = new ViewHolder(view);
+            return viewHolder;
         }
 
         @Override
@@ -37,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return 0;
+
+            return list.size();
         }
 
 
